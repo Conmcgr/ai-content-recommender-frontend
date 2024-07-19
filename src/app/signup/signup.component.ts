134c1f0112287db1c876ecb03f457389dd239233
Conmcgr <cmcgraw@uchicago.edu>
2024-07-19 14:00:48 -0700

    sign up page

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule
+  ],
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent {
+  email: string = '';
+  username: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  errorMessage: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSubmit() {
+    if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'Passwords do not match';
+      return;
+    }
+
+    const signupData = {
+      email: this.email,
+      username: this.username,
+      password: this.password
+    };
+
+    this.http.post<any>('/api/register', signupData).subscribe(
+      response => {
+        this.router.navigate(['/login']);
+      },
+      error => {
+        this.errorMessage = 'User registration failed';
+      }
+    );
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+}
