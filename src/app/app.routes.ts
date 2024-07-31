87420cbd2673a20148b78dcaa66f483e2d50ed8c
Conmcgr <cmcgraw@uchicago.edu>
2024-07-31 11:55:12 -0700

    Built the base for the start session component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { StartSessionComponent } from './start-session/start-session.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'start-session', component: StartSessionComponent },
   // Add other routes here
 ];
--- src/app/start-session/start-session.component.ts
+++ src/app/start-session/start-session.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-start-session',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ],
   templateUrl: './start-session.component.html',
-  styleUrl: './start-session.component.scss'
+  styleUrls: ['./start-session.component.scss']
 })
 export class StartSessionComponent {
+  sessionLength: number = 15;
+  format: string[] = [];
+  contentKeywords: string = '';
 
+  constructor(private router: Router) {}
+
+  adjustSessionLength(amount: number) {
+    const newLength = this.sessionLength + amount;
+    if (newLength >= 5 && newLength <= 60) {
+      this.sessionLength = newLength;
+    }
+  }
+
+  toggleFormat(type: string) {
+    const index = this.format.indexOf(type);
+    if (index > -1) {
+      this.format.splice(index, 1);
+    } else {
+      this.format.push(type);
+    }
+  }
+
+  findContent() {
+    if (this.format.length === 0) {
+      this.format.push('video', 'article', 'podcast'); // Default to all types if none are selected
+    }
+    this.router.navigate(['/show-content'], {
+      queryParams: {
+        sessionLength: this.sessionLength,
+        format: this.format.join(','),
+        keywords: this.contentKeywords
+      }
+    });
+  }
+
+  navigateToSettings() {
+    this.router.navigate(['/settings']);
+  }
 }
